62d45ab8193050050b1a8b2d1d08dea7b1edd516
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-02 16:56:21 +0300

    visitors route add

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,17 @@ const routes = [
   },
   {
     meta: {
+      title: 'Visitors'
+    },
+    path: '/visitors',
+    name: 'visitors',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tables" */ '../views/visitors/Visitors')
+  },
+  {
+    meta: {
       title: 'Tables'
     },
     path: '/tables',

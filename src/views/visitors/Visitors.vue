1c9d041d0d4fbb926e0d2888e0263dcbfefe820b
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-02 16:56:44 +0300

    add visitors component

---
+++ src/views/visitors/Visitors.vue
@@ -0,0 +1,46 @@
+<template>
+  <hero-bar>
+    Visitors
+    <template #right>
+      <router-link to="/" class="button light">
+        Dashboard
+      </router-link>
+    </template>
+  </hero-bar>
+  <main-section>
+
+    <card-component title="Checked-In" :icon="mdiAccountMultiple" has-table>
+      <clients-table checkable/>
+    </card-component>
+  </main-section>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { mdiMonitorCellphone, mdiAccountMultiple, mdiTableBorder, mdiTableOff } from '@mdi/js'
+import MainSection from '@/components/MainSection'
+import ClientsTable from '@/components/ClientsTable'
+import CardComponent from '@/components/CardComponent'
+import HeroBar from '@/components/HeroBar'
+
+export default {
+  name: 'Tables',
+  components: {
+    MainSection,
+    HeroBar,
+    CardComponent,
+    ClientsTable
+  },
+  setup () {
+    const titleStack = ref(['Admin', 'Tables'])
+
+    return {
+      titleStack,
+      mdiMonitorCellphone,
+      mdiAccountMultiple,
+      mdiTableBorder,
+      mdiTableOff
+    }
+  }
+}
+</script>

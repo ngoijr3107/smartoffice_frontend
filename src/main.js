37946a02615587a7b8c43bd67737a12f667b77bc
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-03 11:45:27 +0300

    rename title

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import './css/main.css'
 
 /* Default title tag */
-const defaultDocumentTitle = 'Admin One Vue 3 Tailwind'
+const defaultDocumentTitle = 'Smart Office'
 
 /* Collapse mobile aside menu on route change & set document title from route meta */
 router.afterEach(to => {

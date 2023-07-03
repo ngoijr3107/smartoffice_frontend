91e3bfb31a2978dd1cca88d3c4a6369dc56d57a4
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-03 11:46:32 +0300

    visitors

--- src/views/visitors/Visitors.vue
+++ src/views/visitors/Visitors.vue
@@ -10,37 +10,74 @@
   <main-section>
 
     <card-component title="Checked-In" :icon="mdiAccountMultiple" has-table>
-      <clients-table checkable/>
+      <!-- <clients-table checkable/> -->
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Visit purpose</th>
+            <th>Department</th>
+            <th>Status</th>
+            <th>Check-in Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="visitor in visitorList" :key="visitor.id">
+            <td>{{ visitor.fname }}</td>
+            <td>{{ visitor.lname }}</td>
+            <td>{{ visitor.email }}</td>
+            <td>{{ visitor.phone }}</td>
+            <td>{{ visitor.visit_purpose }}</td>
+            <td>{{ visitor.department }}</td>
+            <td>{{ visitor.status }}</td>
+            <td>{{ visitor.checkintime }}</td>
+          </tr>
+        </tbody>
+      </table>
     </card-component>
   </main-section>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { mdiMonitorCellphone, mdiAccountMultiple, mdiTableBorder, mdiTableOff } from '@mdi/js'
 import MainSection from '@/components/MainSection'
-import ClientsTable from '@/components/ClientsTable'
 import CardComponent from '@/components/CardComponent'
 import HeroBar from '@/components/HeroBar'
+import VisitorService from '@/services/VisitorService'
 
 export default {
   name: 'Tables',
   components: {
     MainSection,
     HeroBar,
-    CardComponent,
-    ClientsTable
+    CardComponent
   },
+  visitorList: [],
   setup () {
     const titleStack = ref(['Admin', 'Tables'])
+    const visitorList = ref([])
+
+    onMounted(async () => {
+      try {
+        visitorList.value = await VisitorService.getVisitors()
+      } catch (error) {
+        console.error(error)
+      }
+    })
 
     return {
       titleStack,
       mdiMonitorCellphone,
       mdiAccountMultiple,
       mdiTableBorder,
-      mdiTableOff
+      mdiTableOff,
+      visitorList
     }
   }
 }
+
 </script>

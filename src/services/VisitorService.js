c76e2766dacd3b06eab0a4c321f01955d50a63a2
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-06 00:34:32 +0300

    modify getVisitors function

--- src/services/VisitorService.js
+++ src/services/VisitorService.js
@@ -1,18 +1,15 @@
 import axios from 'axios'
-import AppConstant from './AppConstant'
+import { AppConstant } from './AppConstant'
 
 export default {
-  methods: {
-    async getVisitors () {
-      try {
-        const response = await axios.get(AppConstant.baseUrl + 'getVisitors')
-        const visitorList = response.data
-        console.log(visitorList)
-        // Do something with the visitorList
-      } catch (error) {
-        console.error(error)
-      }
+  async getVisitors () {
+    try {
+      const response = await axios.get(AppConstant.baseUrl + 'getVisitors')
+      const visitorList = response.data
+      return visitorList
+    } catch (error) {
+      console.error(error)
+      throw error
     }
   }
-  // Other component properties and methods
 }

a61d489dcb104106344b81890743021952f70496
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-07-06 00:35:03 +0300

    add purpose component

---
+++ src/views/configuration_views/Purposes.vue
@@ -0,0 +1,73 @@
+<template>
+  <hero-bar>
+    Visit Purposes
+    <template #right>
+      <router-link to="/" class="button light">
+        Dashboard
+      </router-link>
+    </template>
+  </hero-bar>
+  <main-section>
+
+    <card-component title="All purposes" :icon="mdiTable" has-table>
+      <!-- <clients-table checkable/> -->
+      <table>
+        <thead>
+          <tr>
+            <th>Code</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="purpose in purposeList" :key="purpose.id">
+            <td>{{ purpose.code }}</td>
+            <td>{{ purpose.description }}</td>
+            <!-- <td>{{ visitor.lname }}</td> -->
+          </tr>
+        </tbody>
+      </table>
+    </card-component>
+  </main-section>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import { mdiMonitorCellphone, mdiAccountMultiple, mdiTableBorder, mdiTable } from '@mdi/js'
+import MainSection from '@/components/MainSection'
+import CardComponent from '@/components/CardComponent'
+import HeroBar from '@/components/HeroBar'
+import PurposeService from '@/services/PurposeService'
+
+export default {
+  name: 'Tables',
+  components: {
+    MainSection,
+    HeroBar,
+    CardComponent
+  },
+  purposeList: [],
+  setup () {
+    const titleStack = ref(['Admin', 'Tables'])
+    const purposeList = ref([])
+
+    onMounted(async () => {
+      try {
+        purposeList.value = await PurposeService.getPurpose()
+      } catch (error) {
+        console.error(error)
+      }
+    })
+
+    return {
+      titleStack,
+      mdiMonitorCellphone,
+      mdiAccountMultiple,
+      mdiTableBorder,
+      mdiTable,
+      purposeList
+    }
+  }
+}
+
+</script>
